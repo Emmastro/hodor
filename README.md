2237c014181588af42dda8bc9121967e5b912e90
Emmastro <emurairi18@alastudents.org>
2021-01-04 11:35:40 +0100

    Solve level 0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hodor - cheat online voting contests
+
+Many online contests make gathering votes a part of their process for choosing winners. The problem is that a lot of them are designed with flaws…
---
+++ level_2/level2.py
@@ -0,0 +1,14 @@
+import mechanicalsoup
+
+browser = mechanicalsoup.Browser()
+URL = 'http://158.69.76.135/level2.php'
+
+
+for l in range(1024):
+    page = browser.get(URL)
+    page_html = page.soup
+    form = page_html.select('form')[0]
+    print(form)
+    form.select('input')[0]['value'] = 35
+
+    browser.submit(form, page.url)
